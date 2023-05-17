dee4635897ee5df050d5d2ef4a668ba771a343cc
David Vega <jared.vega@uabc.edu.mx>
2023-05-16 17:01:56 -0700

    Actividad david

---
+++ main.py
@@ -0,0 +1,18 @@
+import colorama 
+
+from art import *
+
+from colorama import init, Fore 
+
+init () 
+
+tprint("art")
+
+aprint("random")
+
+tprint("ejemplo de art.")
+
+print(Fore.BLUE+"El profe",end = " " )
+print(Fore.GREEN+"Se la rifa",end = " " )
+print(Fore.YELLOW+"Lo queremos",end = " " )
+print(Fore.RED+"Mucho",end = " " )
